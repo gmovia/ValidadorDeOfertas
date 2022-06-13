ee10482a4735251e6d9acb95083b348bae0c9565
gmovia27uba <gmovia@fi.uba.ar>
2022-06-13 21:49:15 +0200

    Agrego factoria shopping cart

---
+++ src/factory/shoppingCartFactory.ts
@@ -0,0 +1,31 @@
+import { Calendar } from "../objectRule/calendar";
+import { CalendarFactory } from "./calendarFactory";
+import { PaymentFactory } from "./paymentFactory";
+import { ShoppingCart } from "../shoppingCart/shoppingCart";
+import { ProductFactory } from "./productFactory";
+
+export class ShoppingCartFactory{
+    
+    private productFactory: ProductFactory;
+    private calendarFactory: CalendarFactory;
+    private paymentFactory: PaymentFactory;  
+    
+    constructor(){
+        this.productFactory = new ProductFactory();
+        this.calendarFactory = new CalendarFactory();
+        this.paymentFactory = new PaymentFactory();
+        
+    }
+
+    createShoppingCart(shoppingCart: any): ShoppingCart{
+        const calendar = this.calendarFactory.createCalendar(shoppingCart.purchase_date);
+        const payment = this.paymentFactory.createPayment(shoppingCart.payment);
+        const cart = new ShoppingCart(payment, calendar);
+
+        for(let product of shoppingCart.products){
+            cart.add(this.productFactory.createProduct(product));
+        }
+
+        return cart;
+    }
+}
