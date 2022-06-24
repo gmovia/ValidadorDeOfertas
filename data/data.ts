fa34b2c8f7cd6e3cf8c41943b4b2e7ca2fc33f1f
gmovia27uba <gmovia@fi.uba.ar>
2022-06-24 02:18:51 +0200

    Refactorizo la data

--- data/data.ts
+++ data/data.ts
@@ -5,10 +5,9 @@ import { RuleLiteral } from '../src/type/typeRule';
 
 import untypedProducts from "./dataJSON/products.json";
 import untypedCart from "./dataJSON/shoppingCart.json";
-import untypedRule from "./dataJSON/reglas.json";
-import untypedOffers from "./dataJSON/offers.json";
+import untypedRulesAndOffers from "./dataJSON/ofertasYreglas.json";
 
 export const products: TypeProduct[] = untypedProducts as TypeProduct[];
 export const cart: TypeCart = untypedCart as TypeCart;
-export const rules: RuleLiteral[] = untypedRule as RuleLiteral[];
-export const offers: TypeOffer[] = untypedOffers as TypeOffer[];
+export const rules: RuleLiteral[] = untypedRulesAndOffers.rules as RuleLiteral[];
+export const offers: TypeOffer[] = untypedRulesAndOffers.offers as TypeOffer[];
