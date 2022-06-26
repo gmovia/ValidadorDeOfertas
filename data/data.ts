baea32549187acf9333f7e5289ce905bf1cb7b34
RVLareu <rlareu@fi.uba.ar>
2022-06-26 14:46:49 -0300

    falta ultimo caso anidado

--- data/data.ts
+++ data/data.ts
@@ -5,9 +5,39 @@ import { RuleLiteral } from '../src/type/typeRule';
 
 import untypedProducts from "./dataJSON/products.json";
 import untypedCart from "./dataJSON/shoppingCart.json";
-import untypedRulesAndOffers from "./dataJSON/ofertasYreglas.json";
+import untypedRulesAndOffers from "./dataJSON/offersAndRules.json";
 
 export const products: TypeProduct[] = untypedProducts as TypeProduct[];
 export const cart: TypeCart = untypedCart as TypeCart;
 export const rules: RuleLiteral[] = untypedRulesAndOffers.rules as RuleLiteral[];
 export const offers: TypeOffer[] = untypedRulesAndOffers.offers as TypeOffer[];
+
+
+
+for(let offer of offers){
+    rules.push(offer.rule as RuleLiteral);
+}
+
+untypedRulesAndOffers.offers.forEach(offer => {
+    // element can be a string or an object
+    offer.rule.rules.forEach((element, index) => {
+        //if element is not a string, replace it with its code and push it into rules
+        if(typeof element !== "string"){
+            const rule = element as RuleLiteral
+            rules.push(rule);
+            offer.rule.rules[index] = rule.code;
+        }
+    });
+});
+
+untypedRulesAndOffers.offers.forEach(offer => {
+    // element can be a string or an object
+    offer.rule.rules.forEach((element, index) => {
+        //if element is not a string, replace it with its code and push it into rules
+        if(typeof element !== "string"){
+            const rule = element as RuleLiteral
+            rules.push(rule);
+            offer.rule.rules[index] = rule.code;
+        }
+    });
+});
